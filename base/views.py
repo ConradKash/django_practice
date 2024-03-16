fadc7e6a44f61d92389dfa31d70365b7c88fc608
ConradKash <kakurucon1234@gmail.com>
2024-03-16 16:51:46 +0300

    Add URL patterns and views for home, login, signup, and logout

--- base/views.py
+++ base/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Home Page')
+
+def login(request):
+    return HttpResponse('Login Page')
+
+def signup(request):
+    return HttpResponse('Signup Page')
+
+def logout(request):
+    return HttpResponse('Logout Page')
