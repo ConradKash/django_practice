f5d8437353347c8d962481bdc6c131bdabd4b2ee
ConradKash <kakurucon1234@gmail.com>
2024-03-17 20:14:39 +0300

    Add categories to home view and update template files

--- base/views.py
+++ base/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+categories = [
+    {'id': '1', 'Name': 'Admin'},
+    {'id': '2', 'Name': 'Patient'},
+    {'id': '3', 'Name': 'Pharmacy'},
+    {'id': '4', 'Name': 'Doctor'},
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {'categories':categories}
+    return render(request, 'base\home.html', context)
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'base\login.html')
 
 def signup(request):
     return HttpResponse('Signup Page')
